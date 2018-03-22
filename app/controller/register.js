0f553e65381b03819f4bfcff390b70bf0a1b3137
tsuzukit <colorble@gmail.com>
2018-03-22 18:31:11 +0900

    fix

--- app/controller/register.js
+++ app/controller/register.js
@@ -6,7 +6,7 @@ let post = async (req, res, next) => {
 
   const verificationToken = process.env.SLACK_VERIFICATION_TOKEN;
   if (req.body.token !== verificationToken) {
-    res.status(500).send({ error: 'Something failed!' });
+    res.status(200).send({ text: 'Oops! Something went wrong!' });
     return;
   }
 
@@ -14,19 +14,19 @@ let post = async (req, res, next) => {
   const userName = req.body.user_name;
   const address = req.body.text.replace('0x', '');
   if (address == null || address == '') {
-    res.status(200).send({ text: 'Hello ' + userName + '. Please specify ethereum address!' });
+    res.status(200).send({ text: 'Hello `' + userName + '`. Please specify ethereum address!' });
     return;
   }
   if (!ethereum.isAddress(address)) {
-    res.status(200).send({ text: 'Hello ' + userName + '. ' + address + ' is not valid ethereum address!' });
+    res.status(200).send({ text: 'Hello `' + userName + '`. `' + address + '` is not valid ethereum address!' });
     return;
   }
 
   let result = UserService.createOrUpdate(userId, userName, address);
   if (result) {
-    res.send({'text': 'Hello ' + userName + '. ' + address + ' is registered!'});
+    res.send({'text': 'Hello `' + userName + '`. `' + address + '` is registered!'});
   } else {
-    res.status(500).send({ error: 'Something failed!' });
+    res.status(200).send({ text: 'Oops! Something went wrong!' });
   }
 
 };
