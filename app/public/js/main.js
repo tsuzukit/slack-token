3b6f3d401f9a42a0eb482ffe9198fa5f2aa77911
tsuzukit <colorble@gmail.com>
2018-03-27 23:02:49 +0900

    more on lp

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -27,24 +27,24 @@ $('#nav').affix({
 	// skills chart
 	$(document).ready(function(e) {
 	//var windowBottom = $(window).height();
-	var index=0;
-	$(document).scroll(function(){
-		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
-		if(top<-300){
-			if(index==0){	
-			
-				$('.chart').easyPieChart({
-					easing: 'easeOutBounce',
-					onStep: function(from, to, percent) {
-						$(this.el).find('.percent').text(Math.round(percent));
-					}
-				});
-			
-				}
-			index++;
-		}
-	})
+	// var index=0;
+	// $(document).scroll(function(){
+	// 	var top = $('#skills').height()-$(window).scrollTop();
+	// 	console.log(top)
+	// 	if(top<-300){
+	// 		if(index==0){
+	//
+	// 			$('.chart').easyPieChart({
+	// 				easing: 'easeOutBounce',
+	// 				onStep: function(from, to, percent) {
+	// 					$(this.el).find('.percent').text(Math.round(percent));
+	// 				}
+	// 			});
+	//
+	// 			}
+	// 		index++;
+	// 	}
+	// })
 	//console.log(nagativeValue)
 	});
 
